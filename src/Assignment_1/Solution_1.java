4ae8784812babda67bfa83cd0288897de3b1f210
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-01-31 01:11:39 +0530

    Printing A to Z

---
+++ src/Assignment_1/Solution_1.java
@@ -0,0 +1,9 @@
+package Assignment_1;
+
+public class Solution_1 {
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
