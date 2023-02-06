7f0c6a3788be95991da4e3f453f059d3c9219950
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-02-07 01:35:32 +0530

    Updated Triangle Pattern

--- src/Assignment_1/Solution_2.java
+++ src/Assignment_1/Solution_2.java
@@ -2,10 +2,13 @@ package Assignment_1;
 
 public class Solution_2 {
     public static void main(String[] args) {
-        int row = 5;
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        int n = 9;
+        for (int i = 0; i <= (n - 1) / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i + j == (n - 1) / 2 || j - i == (n - 1) / 2 || i == (n - 1) / 2)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
             }
             System.out.println();
         }
