267b0797ad079cedb8ce1e2a473a615dc1e4a49b
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-02-07 03:01:14 +0530

    Update Solution_2.java

--- src/Assignment_1/Solution_2.java
+++ src/Assignment_1/Solution_2.java
@@ -5,7 +5,7 @@ public class Solution_2 {
         int n = 9;
         for (int i = 0; i <= (n - 1) / 2; i++) {
             for (int j = 0; j < n; j++) {
-                if (i + j == (n - 1) / 2 || j - i == (n - 1) / 2 || i == (n - 1) / 2)
+                if (i + j >= (n - 1) / 2 && j - i <= (n - 1) / 2)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
