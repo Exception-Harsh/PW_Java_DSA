d19b4f1f39ad2465a80a308d147a7f33e47743e5
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-02-12 16:10:54 +0530

    Removing duplicate characters from string.

---
+++ src/Assignment_2/Solution_1.java
@@ -0,0 +1,33 @@
+package Assignment_2;
+
+import java.util.*;
+
+public class Solution_1 {
+    static String removeDuplicate(char[] str, int strSize) {
+        int index = 0;
+
+        for (int i = 0; i < strSize; i++)
+        {
+            int j;
+            for (j = 0; j < i; j++) {
+                if (str[i] == str[j]) {
+                    break;
+                }
+            }
+
+            if (j == i) {
+                str[index++] = str[i];
+            }
+        }
+        return String.valueOf(Arrays.copyOf(str, index));
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String str = scan.nextLine();
+        char[] str1 = str.toCharArray();
+        System.out.println(removeDuplicate(str1, str1.length));
+    }
+}
