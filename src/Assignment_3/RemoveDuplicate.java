29cc7ee9f0a4472bc4898526be3b761840071985
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2024-05-08 13:31:35 +0530

    Program to remove duplicate from a string

---
+++ src/Assignment_3/RemoveDuplicate.java
@@ -0,0 +1,18 @@
+package Assignment_3;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        String s1 = "Succession";
+        s1 = s1.toUpperCase();
+        String s2 = "";
+        String ch;
+
+        for (int i = 0; i < s1.length(); i++) {
+            ch = "" + s1.charAt(i);
+            if(s2.contains(ch))
+                continue;
+            s2 += ch;
+        }
+        System.out.print(s2);
+    }
+}
